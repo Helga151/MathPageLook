12a8aef1749901b55da8da644f42dd1a65ec1bea
Helga151 <82333349+Helga151@users.noreply.github.com>
2021-08-20 15:39:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const nav = document.querySelector('.navigation');
+const topOfNav = nav.offsetTop;
+const toggleMenu = document.querySelector('.hamburger');
+
+function navScroll() {
+    if (topOfNav <= window.scrollY) {
+        console.log('teraz');
+        nav.classList.add('fixed');
+    } else {
+        nav.classList.remove('fixed');    
+    }
+}
+
+let activeMenu = true;
+toggleMenu.classList.add('hamburger--active'); //start with cross 
+
+function slideMenu() {
+    console.log(nav.offsetWidth, toggleMenu.offsetWidth);
+    if(activeMenu) {
+        nav.style.transform = `translateX(-${nav.offsetWidth - toggleMenu.offsetWidth - 25}px)`; //obliczyć padding
+        activeMenu = false;
+        toggleMenu.classList.remove('hamburger--active'); 
+        nav.classList.add('rolledUp');
+    }
+    else {
+        nav.style.transform = `translateX(0)`;
+        activeMenu = true;
+        toggleMenu.classList.add('hamburger--active');
+        nav.classList.remove('rolledUp');
+    }
+}
+
+window.addEventListener('scroll', navScroll);
+toggleMenu.addEventListener('click', slideMenu)
